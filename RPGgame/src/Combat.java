e268e9af35a38c4fe5ac46a44ec120e885772d0b
Rai91 <raimonbr@gmail.com>
2015-02-03 15:04:55 +0100

    03/02
    Code.

---
+++ RPGgame/src/Combat.java
@@ -0,0 +1,5 @@
+/**
+ * Created by rari on 03-02-2015.
+ */
+public class Combat {
+}
--- RPGgame/src/map.java
+++ RPGgame/src/map.java
@@ -1,16 +1,52 @@
-class map {
-    int startX;
-    int startY;
-    int end;
+import java.util.*;
+import java.io.*;
 
-    String[] walls;
-    String[] freeSpaces;
+class map {
+    private int playerX;
+    private int playerY;
+    private int end;
+
     char[][] gameMap;
 
-    static char[][] putWallsMap(String[] walls, char[][] gameMap) {
+    public int getPlayerX(){
+        return this.playerX;
+    }
 
-        char[][] mapWalls;
-        mapWalls = gameMap;
+    public void setPlayerX(int playerX){
+        this.playerX= playerX;
+    }
+
+    public int getPlayerY(){
+        return this.playerY;
+    }
+
+    public void setPlayerY(int playerY){
+        this.playerY= playerY;
+    }
+
+    public int getEnd(){
+        return this.end;
+    }
+
+    public void setEnd(int end){
+        this.end = end;
+    }
+
+    public char getPosition(int x, int y){
+        return gameMap[x][y];
+    }
+
+    public map (){
+        super();
+        gameMap = new char[7][7];
+        for(int i=0;i<7;i++){
+            for(int j=0;j<7;j++) {
+                gameMap[i][j]= 'H';
+            }
+        }
+    }
+
+    public char[][] putWallsMap(String[] walls) {
 
         for (int x = 0; x < walls.length; x++) {
             char charWallX = walls[x].charAt(0);
@@ -19,15 +55,12 @@ class map {
             int wallX = Character.getNumericValue(charWallX);
             int wallY = Character.getNumericValue(charWallY);
 
-            mapWalls[wallX][wallY] = 'W';
+            this.gameMap[wallX][wallY] = 'W';
 
         }
-        return mapWalls;
+        return gameMap;
     }
-    static char[][] putFreeSpaces(String[] freeSpaces, char[][] gameMap) {
-
-        char[][] mapFreeSpaces;
-        mapFreeSpaces = gameMap;
+    public char[][] putFreeSpaces(String[] freeSpaces) {
 
         for (int x = 0; x < freeSpaces.length; x++) {
             char charWallX = freeSpaces[x].charAt(0);
@@ -35,41 +68,65 @@ class map {
 
             int freeX = Character.getNumericValue(charWallX);
             int freeY = Character.getNumericValue(charWallY);
-            if (mapFreeSpaces[freeX][freeY] == 0) {
-                mapFreeSpaces[freeX][freeY] = 'H';
+            if (gameMap[freeX][freeY] == 0) {
+                gameMap[freeX][freeY] = 'H';
             }
 
         }
-        return mapFreeSpaces;
+        return gameMap;
     }
-    static char[][] putPlayerMap(int x, int y, char[][] gameMap) {
+    public char[][] putPlayerMap(int x, int y) {
 
-        char[][] mapPlayer;
-        mapPlayer = gameMap;
+        gameMap[x][y] = 'P';
 
-
-        mapPlayer[x][y] = 'P';
-
-        return mapPlayer;
+        return gameMap;
     }
-    static char[][] putOcup(int x, int y, char[][] gameMap) {
 
-        char[][] mapOcup;
-        mapOcup = gameMap;
+    public char [][] randomMonsters() {
 
+        String [] spacesAvailable = new String [18];
+        int monsterPositionVector = 0;
+        Random  random = new Random();
+        for(int i=0;i<gameMap.length;i++){
+            for(int j=0;j<gameMap.length;j++) {
+                if (gameMap[i][j] == 'H') {
+                    spacesAvailable[monsterPositionVector]= Integer.toString(i*10 +j);
+                    System.out.println(spacesAvailable[monsterPositionVector] + " ");
+                    monsterPositionVector++;
+                }
+            }
+        }
 
-        mapOcup[x][y] = 'O';
+        int monsterXY = Integer.valueOf(spacesAvailable[random.nextInt(18)]);
+        char charMonsterX = spacesAvailable[monsterXY].charAt(0);
+        char charMonsterY = spacesAvailable[monsterXY].charAt(1);
 
-        return mapOcup;
+        int monsterX = Character.getNumericValue(charMonsterX);
+        int monsterY = Character.getNumericValue(charMonsterY);
+
+        this.gameMap[monsterX][monsterY]= 'M';
+
+        return gameMap;
     }
-    static void printMap(char gameMap[][]){
 
-        for (int x = 0; x < 7; x++) {
+    public char[][] putOcup(int x, int y) {
+
+        gameMap[x][y] = 'O';
+
+        return gameMap;
+    }
+    public void printMap(){
+
+             System.out.println();
+
+            for (int x = 0; x < 7; x++) {
             for (int y = 0; y < 7; y++) {
                 System.out.print(gameMap[x][y] + " ");
             }
             System.out.println();
         }
+
+
     }
 
 
